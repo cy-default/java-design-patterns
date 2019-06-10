ac1f920e1079103bdfbb0efcf679ac00565539d3
chenyuan <chen.yuan135@chinaredstar.com>
2019-06-10 20:48:18 +0800

    添加注释

--- src/main/java/com/remotes/design/decorator/DrinkMilkTea.java
+++ src/main/java/com/remotes/design/decorator/DrinkMilkTea.java
@@ -5,6 +5,7 @@ package com.remotes.design.decorator;
  * @author yuan.chen
  * @email chen.yuan135@chinaredstar.com
  * @Date 2019-06-10
+ * 文章： https://www.jianshu.com/p/427342d3b5c0
  */
 public interface DrinkMilkTea {
 
